dd5cd508e211082ba952cbe36df78a2f78360b6b
RainChen <hirainchen@gmail.com>
2016-10-19 11:07:57 +0800

    add Goto: Chrome Web Store

--- js/commands.js
+++ js/commands.js
@@ -21,6 +21,7 @@ _suggestions = [
 	{ 'caption': 'Goto: History', 'command': 'openNewTab', 'args': {'url': 'chrome://history/'}, 'shortcut': {'windows': ['Ctrl','H'], 'mac': ['⌘','H']} },
 	{ 'caption': 'Goto: Settings', 'command': 'openNewTab', 'args': {'url': 'chrome://settings/'} },
 	{ 'caption': 'Goto: Keyboard Shortcuts Setting', 'command': 'openNewTab', 'args': {'url': 'chrome://extensions/configureCommands'} },
+	{ 'caption': 'Goto: Chrome Web Store', 'command': 'openNewTab', 'args': {'url': 'https://chrome.google.com/webstore/category/extensions'} },
 	{ 'caption': 'Page: Print', 'command': 'printPage', 'shortcut': {'windows': ['Ctrl','P'], 'mac': ['⌘','P']} },
 	{ 'caption': 'Tab: Close Current', 'command': 'closeCurrentTab', 'shortcut': {'windows': ['Ctrl','W'], 'mac': ['⌘','W']} },
 	{ 'caption': 'Tab: Close All Others', 'command': 'closeAllOtherTabs' },
